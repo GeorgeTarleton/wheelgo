7bd84d0cceb596b80844de31caedf7e7dfc5b96b
George Tarleton <40340336+Geophage@users.noreply.github.com>
2022-11-02 12:09:19 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/wheelgo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wheelgo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter Demo',flutter
+      theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const MainMap()
+    );
+  }
+}
+
+class MainMap extends StatefulWidget {
+  const MainMap({super.key});
+
+  @override
+  State<MainMap> createState() => _MainMapState();
+}
+
+class _MainMapState extends State<MainMap> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.509364, -0.128928),
+          zoom: 9.2,
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [],
+      ),
+    );
+  }
+}
