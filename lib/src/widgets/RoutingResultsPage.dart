943ef806ed751ba7a8700671833ec371fa109f57
George Tarleton <40340336+Geophage@users.noreply.github.com>
2022-12-18 17:16:50 +0000

    Extracted routing info parts out into components

--- lib/src/widgets/RoutingResultsPage.dart
+++ lib/src/widgets/RoutingResultsPage.dart
@@ -8,112 +8,158 @@ class RoutingResultsPage extends StatelessWidget {
 
     return Column(
         children: [
-          Container(
-            child: Text("First info part"),
-          ),
+          KeyInfo(),
           Container(
             child: Column(
               children: [
-                // TODO Extract this out into a start component
-                Container(
-                  child: Row(
-                    children: [
-                      Text("Start icon"),
-                      Text("Start location")
-                    ],
-                  ),
-                ),
+                StartInfo(),
                 Container(
                   child: Text("Divider buffer"),
                 ),
-                // TODO Extract this into its own walking component
-                Container(
-                  child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text("Wheeling icon"),
-                            Text("Wheeling time and distance")
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text("Divider"),
-                            Text("Directions dropdown"),
-                          ],
-                        )
-                      ],
-                  ),
-                ),
-                // TODO Extract this to its own public transport component
-                Container(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text("Public transport icon"),
-                          Text("Station at time")
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("Divider"),
-                          Column(
-                            children: [
-                              Text("Line 1 to Line 2 for X mins"),
-                              Text("Stops dropdown")
-                            ],
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("Public transport icon"),
-                          Text("Line 2 to Line 3 for X mins"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("Divider"),
-                          Text("Stops dropdown"),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                // TODO Extract this out into an arrival component
-                Container(
-                  child: Row(
-                    children: [
-                      Text("Arrival icon"),
-                      Text("Arrived at X at time")
-                    ],
-                  ),
-                ),
-                // TODO Extract out into an elevation component
-                Container(
-                  child: Row(
-                    children: [
-                      Row(
-                        children: [
-                          Text("Up icon"),
-                          Text("X m")
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("Down icon"),
-                          Text("X m")
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                WalkingInfo(),
+                PublicTransportInfo(),
+                ArrivalInfo(),
+                ElevationInfo(),
               ],
             ),
           )
         ]
     );
   }
+}
 
+class KeyInfo extends StatelessWidget {
+  const KeyInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("First info part"),
+    );
+  }
+}
+
+class StartInfo extends StatelessWidget {
+  const StartInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text("Start icon"),
+          Text("Start location")
+        ],
+      ),
+    );
+  }
+}
+
+class WalkingInfo extends StatelessWidget {
+  const WalkingInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text("Wheeling icon"),
+              Text("Wheeling time and distance")
+            ],
+          ),
+          Row(
+            children: [
+              Text("Divider"),
+              Text("Directions dropdown"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PublicTransportInfo extends StatelessWidget {
+  const PublicTransportInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text("Public transport icon"),
+              Text("Station at time")
+            ],
+          ),
+          Row(
+            children: [
+              Text("Divider"),
+              Column(
+                children: [
+                  Text("Line 1 to Line 2 for X mins"),
+                  Text("Stops dropdown")
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text("Public transport icon"),
+              Text("Line 2 to Line 3 for X mins"),
+            ],
+          ),
+          Row(
+            children: [
+              Text("Divider"),
+              Text("Stops dropdown"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ArrivalInfo extends StatelessWidget {
+  const ArrivalInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text("Arrival icon"),
+          Text("Arrived at X at time")
+        ],
+      ),
+    );
+  }
+}
+
+class ElevationInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Row(
+            children: [
+              Text("Up icon"),
+              Text("X m")
+            ],
+          ),
+          Row(
+            children: [
+              Text("Down icon"),
+              Text("X m")
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
